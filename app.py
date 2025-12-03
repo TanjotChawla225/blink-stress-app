20d6c7840faacfeba4c7632da3357c0ad74b8dc3
TanjotChawla225 <chawlatanjot7623@gmail.com>
2025-12-03 14:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import joblib
 import tempfile
 from feature_extraction import extract_aperture_from_video, extract_blink_features
 
-st.title("Blink-Based Emotion Detection App(high or low arousal emotions)")
-st.write(" High Arousal like admiration , amusement, excitement , fear and Low Arousal like boredom , confusion, calmness, relief")
+st.title("Blink-Based Emotion Detection App(High or Low Intensity emotions)")
+st.write(" High Intensity like admiration , amusement, excitement , fear and Low Intensity like boredom , confusion, calmness, relief")
 st.write("Upload a 4–5 second video showing your face and eyes.")
 
 uploaded_file = st.file_uploader("Upload video", type=["mp4", "mov", "avi"])
@@ -36,6 +36,6 @@ if uploaded_file is not None:
 
     st.subheader("Prediction")
     if pred == 1:
-        st.error(" HIGH AROUSAL !!")
+        st.error(" HIGH INTENSITY !!")
     else:
-        st.success("LOW AROUSAL :D")
+        st.success("LOW INTENSITY :D")
